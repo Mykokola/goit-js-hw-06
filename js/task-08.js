d09d576ca644fb988bb7c10b81edc5660c1b7b90
Mykokola <j0kfrbr@gmail.com>
2023-04-18 10:50:40 -0500

    Update task-08.js

--- js/task-08.js
+++ js/task-08.js
@@ -6,9 +6,9 @@ buttonSubmit.addEventListener("submit", (e) => {
   if (emailForm.value == "" || passwordForm.value == "") {
     alert("всі поля повинні бути заповнені");
   } else {
-    let resultEmail = emailForm.attributes.name.value;
-    let resultpassword = passwordForm.attributes.name.value;
-    let result = {};
+    const resultEmail = emailForm.attributes.name.value;
+    const resultpassword = passwordForm.attributes.name.value;
+    const result = {};
     result[resultEmail] = emailForm.value;
     result[resultpassword] = passwordForm.value;
     console.log(result);
